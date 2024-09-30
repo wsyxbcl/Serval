c05f4b2e7a15b980e7d86a414bd8c946974d85a2
wsyxbcl <wsyxbcl@gmail.com>
2024-09-30 18:00:48 +0800

    binary for serval xmp

---
+++ bin/serval-xmp-extract.rs
@@ -0,0 +1,14 @@
+use std::env;
+use anyhow::Result;
+
+use serval::utils::copy_xmp;
+
+fn main() -> Result<()> {
+    let source_dir = env::current_dir()?;
+
+    let mut output_dir = source_dir.clone();
+    output_dir.push("xmp");
+
+    copy_xmp(source_dir, output_dir)?;
+    Ok(())
+}
